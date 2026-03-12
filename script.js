ad7599343d40507938e71387625e72b732b8b8d6
Prince Goyal <0231CSE137@niet.co.in>
2026-03-12 10:56:00 +0530

    Add function to update and display current time

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function updateTime(){
 
+let now = new Date()
+
+let time = now.toLocaleTimeString()
+
+let date = now.toLocaleDateString()
+
+document.getElementById("time").innerHTML =
+"Current Time: " + time + " | " + date
+
+}
+
+setInterval(updateTime,1000)
